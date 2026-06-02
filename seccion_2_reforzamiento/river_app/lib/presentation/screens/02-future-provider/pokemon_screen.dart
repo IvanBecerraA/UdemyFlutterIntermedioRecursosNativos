c1dbfd9871d9105198c9087121a4f8d6e14b2728
IvanBecerraA <ivanbecerra91@gmail.com>
2026-06-01 23:43:36 -0400

    sección 2: 17. Consumir un Future Provider

--- seccion_2_reforzamiento/river_app/lib/presentation/screens/02-future-provider/pokemon_screen.dart
+++ seccion_2_reforzamiento/river_app/lib/presentation/screens/02-future-provider/pokemon_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_app/presentation/providers/providers.dart';
 
 
 
-class PokemonScreen extends StatelessWidget {
+class PokemonScreen extends ConsumerWidget {
   const PokemonScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final pokemonAsync = ref.watch(pokemonNameProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Future Provider'),
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Center(
+        child: pokemonAsync.when(
+          data: (value) => Text(value), 
+          error: (error, stackTrace) => Text('Error: $error'), 
+          loading: () => const CircularProgressIndicator(),
+        ),
       ),
     ); 
   }
