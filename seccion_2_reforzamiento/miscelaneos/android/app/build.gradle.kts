f66271ac7ec3db3b4e1e144955c7d4d10d1d1b99
IvanBecerraA <ivanbecerra91@gmail.com>
2026-06-03 23:38:58 -0400

    sección 3: 35. Permission Handler - Android

--- seccion_2_reforzamiento/miscelaneos/android/app/build.gradle.kts
+++ seccion_2_reforzamiento/miscelaneos/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.miscelaneos"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
