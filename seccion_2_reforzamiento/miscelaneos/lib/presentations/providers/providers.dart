9a277877af6d88ff7ae10f646c23693de193fe90
IvanBecerraA <ivanbecerra91@gmail.com>
2026-06-06 19:18:19 -0400

    sección 4: 51. Magnetómetro - Provider

--- seccion_2_reforzamiento/miscelaneos/lib/presentations/providers/providers.dart
+++ seccion_2_reforzamiento/miscelaneos/lib/presentations/providers/providers.dart
@@ -4,3 +4,4 @@ export 'permissions/permissions_provider.dart';
 // ! Sensors
 export 'sensors/gyroscope_provider.dart';
 export 'sensors/accelerometer_providers.dart';
+export 'sensors/magnetometer_providers.dart';
