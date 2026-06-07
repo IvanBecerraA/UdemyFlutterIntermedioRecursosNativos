bf1bc2ff6ff36c0cb3ee03f6b3ce31a48c673d9e
IvanBecerraA <ivanbecerra91@gmail.com>
2026-06-06 22:39:18 -0400

    sección 4: 54. Brújula - Pantalla inicial

--- seccion_2_reforzamiento/miscelaneos/lib/presentations/screens/sensors/compass_screen.dart
+++ seccion_2_reforzamiento/miscelaneos/lib/presentations/screens/sensors/compass_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentations/providers/providers.dart';
+import 'package:miscelaneos/presentations/screens/screens.dart';
 
 
-class CompassScreen extends StatelessWidget {
+class CompassScreen extends ConsumerWidget {
   const CompassScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+
+    final locationGranted = ref.watch(permissionsProvider).locationGranted;
+
+    if (!locationGranted) {
+      return const AskLocationScreen();
+    }
+
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('CompassScreen'),
+        title: const Text('Brújula', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.white),
       ),
       body: const Center(
         child: Text('Hola'),
@@ -16,3 +29,6 @@ class CompassScreen extends StatelessWidget {
     );
   }
 }
+
+
+
