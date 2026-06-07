fb48a6d46a2f8f229a6b8b9dbbf272a78d40cd98
IvanBecerraA <ivanbecerra91@gmail.com>
2026-06-07 13:54:25 -0400

    sección 4: 72. Mapeo y respuesta

---
+++ seccion_2_reforzamiento/miscelaneos/lib/domain/infraestructure/repositories/pokemons_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:miscelaneos/domain/domain.dart';
+import 'package:miscelaneos/domain/infraestructure/datasources/pokemons_datasource_impl.dart';
+
+
+
+class PokemonsRepositoryImpl implements PokemonsRepository{
+
+  final PokemonsDatasource datasource;
+
+  PokemonsRepositoryImpl(PokemonsDatasource? datasource)
+    : datasource = datasource ?? PokemonsDatasourceImpl();
+
+
+  @override
+  Future<(Pokemon?, String)> getPokemon(String id) async {
+    return datasource.getPokemon(id);
+  }  
+
+}
