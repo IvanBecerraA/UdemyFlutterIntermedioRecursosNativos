29bacaca0b4257cd80cbb984f40ad5cf5e2f5f4d
IvanBecerraA <ivanbecerra91@gmail.com>
2026-06-08 21:22:29 -0400

    sección 10: 115. Diseño de pantalla

--- seccion_2_reforzamiento/miscelaneos/lib/presentations/screens/locations/controlled_map_screen.dart
+++ seccion_2_reforzamiento/miscelaneos/lib/presentations/screens/locations/controlled_map_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:miscelaneos/presentations/providers/providers.dart';
 
 
@@ -13,9 +15,6 @@ class ControlledMapScreen extends ConsumerWidget {
     final watchUserLocation = ref.watch(watchLocationProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ControlledMapScreen'),
-      ),
       body: watchUserLocation.when(
         data: (data) => MapAndControls(latitude: data.$1, longitude: data.$2,), 
         error: (error, stackTrace) => Text('$error'), 
@@ -35,6 +34,93 @@ class MapAndControls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        
+        _MapView(initialLat: latitude, initialLng: longitude,),
+
+        // ! Boton para salir
+        Positioned(
+          top: 40, 
+          left: 20,
+          child: IconButton.filled(
+            onPressed: () {
+              context.pop();
+            }, 
+            icon: const Icon(Icons.arrow_back)
+          )
+        ),
+
+        // ! Ir a la posición del usuario
+        Positioned(
+          bottom: 40, 
+          left: 20,
+          child: IconButton.filled(
+            onPressed: () {
+              
+            }, 
+            icon: const Icon(Icons.location_searching)
+          )
+        ),
+
+        // ! Seguir usuario
+        Positioned(
+          bottom: 90, 
+          left: 20,
+          child: IconButton.filled(
+            onPressed: () {
+              
+            }, 
+            icon: const Icon(Icons.accessibility_new_outlined)
+          )
+        ),
+
+        // ! Crear marcador
+        Positioned(
+          bottom: 140, 
+          left: 20,
+          child: IconButton.filled(
+            onPressed: () {
+              
+            }, 
+            icon: const Icon(Icons.directions_run)
+          )
+        ),
+
+      ],
+    );
+  }
+}
+
+
+class _MapView extends StatefulWidget {
+
+  final double initialLat;
+  final double initialLng;
+
+  const _MapView({required this.initialLat, required this.initialLng});
+
+  @override
+  State<_MapView> createState() => __MapViewState();
+}
+
+class __MapViewState extends State<_MapView> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(widget.initialLat, widget.initialLng),
+          zoom: 12
+        ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        onMapCreated: (GoogleMapController controller) {
+          // _controller.complete(controller);
+        },
+      );
   }
 }
